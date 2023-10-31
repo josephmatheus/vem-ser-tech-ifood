0066a24a6d7e696917beea12b92705c89a8e0250
Joseph Matheus <josephmatheus@hotmail.com>
2023-10-31 11:00:16 -0300

    Adiciona arquivos aula 01

---
+++ modulo02-logica-de-programacao-i/aula-01/aula-01.js
@@ -0,0 +1,23 @@
+// undefined
+let saida;
+// string
+saida = "Joseph";
+// null
+saida = null;
+// number
+saida = 5;
+// NaN
+saida = "texto" - 5;
+// boolean
+saida = true;
+
+saida = "angular";
+
+// console.log(saida.length); // 7
+
+let capitalized = saida.charAt(0).toUpperCase() + saida.slice(1);
+
+console.log(capitalized);
+
+
+
