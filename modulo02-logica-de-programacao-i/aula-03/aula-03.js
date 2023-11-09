c1dc330dd2f62c1a23e61a7cfdc2a96b2125f539
Joseph Matheus <josephmatheus@hotmail.com>
2023-11-08 22:02:07 -0300

    Complemento aula 3

--- modulo02-logica-de-programacao-i/aula-03/aula-03.js
+++ modulo02-logica-de-programacao-i/aula-03/aula-03.js
@@ -14,56 +14,92 @@ saida = meuArray.push("Vue");
 saida = meuArray.unshift("Next");
 
 // copiar arrays
-    // saida = meuArray.slice();
-    // saida.pop()
+// saida = meuArray.slice();
+// saida.pop()
 
-    saida = meuArray;
-    saida.pop();
+saida = meuArray;
+saida.pop();
 
 // unir arrays
-    saida = [...meuArray, ...meuArray];
-
+saida = [...meuArray, ...meuArray];
 
 // console.log("Array de saída: ", saida);
 // console.log("Array original: ", meuArray);
 
 // OBJETOS
 
-    const pessoa = {
-      "Primeiro Nome": "Joseph",
-      ultimoNome: "Lima",
-      idade: 25,
-      corOlhos: "Castanho-escuro",
-      habilidades: ["Javascript", "HTML", "CSS"],
-      níveis: {
-        css: 8,
-        javascript: 10,
-        html: 10,
-      },
-    };
+const pessoa = {
+  "Primeiro Nome": "Joseph",
+  ultimoNome: "Lima",
+  idade: 25,
+  corOlhos: "Castanho-escuro",
+  habilidades: ["Javascript", "HTML", "CSS"],
+  níveis: {
+    css: 8,
+    javascript: 10,
+    html: 10,
+  },
+};
 
-    // saida = pessoas.primeiroNome;
-    // saida = pessoa["primeiroNome"];
-    // pessoa.idade = 28;
-    // pessoa.altura = 1.78;
+// saida = pessoas.primeiroNome;
+// saida = pessoa["primeiroNome"];
+// pessoa.idade = 28;
+// pessoa.altura = 1.78;
 
-    // saida = Object.keys(pessoa);
+// saida = Object.keys(pessoa);
 
-    // Desestruturação
-    // const { idade: _idade } = pessoa; // mudando o nome de uma variável
+// Desestruturação
+// const { idade: _idade } = pessoa; // mudando o nome de uma variável
 
+// console.log("cadastro inicial: ", pessoas);
 
-    console.log("cadastro inicial: ", pessoas);
+let resposta;
 
-    let resposta;
+// resposta = prompt("Add pessoa1");
+const { ...usuario1 } = pessoa;
+usuario1["Primeiro Nome"] = resposta;
+pessoas.push(usuario1);
 
-    resposta = prompt("Add pessoa1");
-    const {...usuario1} = pessoa;
-    usuario1["Primeiro Nome"] = resposta;
-    pessoas.push(usuario1);
+// resposta = prompt("Add pessoa2");
 
+// console.log("usuarios cadastrados: ", pessoas);
 
-    // resposta = prompt("Add pessoa2");
+// OPERADORES DE COMPARAÇÃO
+// ==, !=, >, <, >=, <=, ===
 
+saida = 5 == 5;
+saida = 5 != 5;
+saida = "murilo" == "Murilo";
+saida = 10 > 10;
+saida = 10 < 10;
+saida = 10 >= 10;
+saida = 10 <= 10;
 
-    console.log("usuarios cadastrados: ", pessoas);
+saida = 10 == 10; //true
+saida = 10 == "10"; //true
+saida = 10 === "10"; //false
+
+//exercicio, verificar se numero é par
+const meuNum = 5;
+saida = meuNum % 2 === 0;
+
+// OPERADORES LÓGICOS
+// ||, &&
+
+saida = 2 == 2 && 4 > 5; // false
+saida = 2 == 2 && 4 < 5; // true
+
+saida = 2 == 2 || 4 > 5; // true
+
+saida = !(2 == 2) || 4 > 5; // false
+
+// Exemplo de uso do NOT - toggle
+let estado;
+estado = !estado;
+
+//exercicio - verificar se uma palavra inicia com a vogal
+const palavra = "Apple";
+const letra = palavra.charAt(0).toLowerCase();
+const vogal = "a" == letra || "e" == letra || "i" == letra || "o" == letra || "u" == letra;
+
+console.log("SAÍDA: ", vogal);
