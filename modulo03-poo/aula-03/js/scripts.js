f5400e2fcefd8740e8a230627b6657e7eaf890da
Joseph Matheus <josephmatheus@hotmail.com>
2023-12-04 21:55:36 -0300

    Adiciona arquivos aula 3

---
+++ modulo03-poo/aula-03/js/scripts.js
@@ -0,0 +1,11 @@
+console.log("===== Aula 03 =====");
+
+const retangulo = new Retangulo(10, 20);
+
+retangulo.altura = 15;
+retangulo.base = 30;
+
+console.log(retangulo);
+
+console.log("Altura: ", retangulo.altura);
+console.log("Base: ", retangulo.base);
